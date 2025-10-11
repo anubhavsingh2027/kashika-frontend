6d89742deb35c64580c45fd08bf44d0b06ccb09a
anubhavsingh2027 <anubhavsingh2027@gmail.com>
2025-10-12 00:56:57 +0530

    improve access

---
+++ assets/js/hjhghgvhgvhgvhgvhgvhAacessAghgghbhgg.js
@@ -0,0 +1,18 @@
+async function  checkAccess (){
+  try {
+    const response = await fetch(`https://kashika-backend.onrender.com/kashikaTravel/admin`,{
+        method: "GET",
+        credentials: "include",
+      }
+);
+    const data = await response.json();
+    if(!data.access){
+      window.location.href="unauthorised.html";
+    }
+  } catch (error) {
+    console.log("error");
+    window.location.href="index.html"
+  }
+}
+
+document.addEventListener("DOMContentLoaded",checkAccess);
