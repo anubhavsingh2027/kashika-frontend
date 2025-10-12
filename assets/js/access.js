12ff0f15dacf79fe14c37538ba7fff3b8ccb2618
anubhavsingh2027 <anubhavsingh2027@gmail.com>
2025-10-12 10:16:13 +0530

    improve user history

--- assets/js/access.js
+++ assets/js/access.js
@@ -8,7 +8,12 @@ async function  checkAccess (){
     const data = await response.json();
     console.log("data :",data);
     if(!data.access){
-      window.location.href="unauthorised403.html";
+      setTimeout(()=>{
+        
+        console.log("user session :",d);
+        console.log("data from access :",data)
+        window.location.href="unauthorised403.html";
+      },10000)
     }
   } catch (error) {
     console.log("error");
